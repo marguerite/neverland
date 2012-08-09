39648be88a52e96b570610c08e3589644645cb1f
Weng Xuetian <wengxt@gmail.com>
2012-08-09 19:59:45 +0800

    update neverland

--- neverland/js/neverland.js
+++ neverland/js/neverland.js
@@ -29,17 +29,21 @@ $(document).ready(function(){
 	});
 	
 	// 柱状图的高度计算
-	var max_width = 0;
+	var max_number = 0;
 	$('.bar-chart td.cat_bar').each(function (i, n) {
-	if (max_width < $(n).width()) {
-	max_width = $(n).width();
-	}
+		var number = parseInt($(this)[0].innerHTML);
+		if (number > max_number) {
+			max_number = number;
+		}
+		$(this).css("min-width", "10px");
 	});
-	var max_number = max_width/5;
-	var width_average = ($(".bar-chart").width() - $(".bar-chart td.cat_name").width())/max_number;
+	if (max_number <= 0)
+		max_number = 1;
 	$(".bar-chart td.cat_bar").each(function(){
-		var width_mutiple = $(this).width()/5;
-		$(this).width(width_mutiple*width_average);
+		var number = parseInt($(this)[0].innerHTML);
+		if (number <= 0)
+			number = 1;
+		$(this).css("width", (number / max_number * 100) + "%");
 	});
 	
 	// 匹配「KDE提交摘要*」，把它的 p 换成外面包上 ul li
@@ -58,69 +62,6 @@ $(document).ready(function(){
 		}
 	});
 	
-	// 标签云的正圆
-	$("a[class^=tag-link]").each(function(i, obj){
-
-		var clone;
-		var position;
-		clone = $(obj).clone();
-
-		/*
-		//得到clone的宽度并增加一些呼吸空间
-		var cloneWidth = $(obj).width() + 20;
-		var cloneHeight = $(obj).height() + 20;
-		*/
-		
-		/* 算法说明
-			首先圆有宽度 10% 的 padding，这样两边一共是 20%，圆的实际宽度是 cloneWidth*1.2;
-			elements 的 position 都是取最左上角一点的。于是要让圆看起来位置没变化：
-			它的宽度就应该往左移动 cloneWidth*0.1;
-			高度有点复杂，它应该向上移动 (现在的圆高度/2 - 原来的高度/2 );
-			现在的圆高度由于有 padding，是 cloneHeight*1.2;
-			原来的高度是 cloneHeight;
-			但是原来的 element 是个长方形，现在的 element 是个圆，直径等于 cloneWidth*1.2;
-			因为有 css("height", cloneWidth)，刚克隆来的原来的高度被抹掉了，于是我们需要一个变量来记住它:
-			$(clone).data("heightsave",cloneHeight);
-			以上，圆的 position 调整变为：
-			top = position.top - (cloneWidth*1.2 - $(clone).data("heightsave"))/2;
-			left = position.left - cloneWidth*0.1;
-		*/
-		
-		var cloneWidth = $(obj).width();
-		var cloneHeight = $(obj).height();
-		$(clone).data("heightsave",cloneHeight);
-		
-		$(clone).addClass("clonedItem");
-		position = $(obj).position();
-
-		$(obj).bind("mouseover", function(e){
-
-			$(".tagcloud a").css("z-index","1");
-
-			$(clone).css("height", cloneWidth).css("width", cloneWidth).css("z-index", 1000).css("line-height", cloneWidth + 'px');
-			$(clone).css({"background-color":"#0068c6","color":"white"});
-			
-			//clone 的 padding+radius
-			$(clone).css("padding", cloneWidth*0.1).css("border-radius", cloneWidth*1.2 / 2 + 'px').css("-webkit-border-radius", cloneWidth*1.2 / 2 + 'px').css("-khtml-border-radius", cloneWidth*1.2 / 2 + 'px').css("-moz-border-radius", cloneWidth*1.2 / 2 + 'px');
-			
-			//clone 的 position
-			$(clone).css("top", position.top - (cloneWidth*1.2 - $(clone).data("heightsave"))/2);
-			$(clone).css("left", position.left - cloneWidth*0.1);
-			
-			$(clone).appendTo($(this).parents(".tagcloud")).css("position", "absolute");
-			
-
-			$(clone).bind("mouseout", function(e){
-				$(".clonedItem").remove(); //防止鼠标过快移动无法清除一些背景
-				$(clone).remove();
-				// 清除 z-index
-				$(".tagcloud a").css("z-index","");
-			});
-
-		}); 
-
-	}); // end each
-	
 	// 删除 translate 插件那条奇丑无比的线
 	$(".translate_hr").remove();
 	
@@ -132,4 +73,5 @@ $(document).ready(function(){
 	$("pre.dekstop").snippet("desktop",{style:"acid"});
 	$("pre.changelog").snippet("changelog",{style:"acid"});
 	$("pre.log").snippet("log",{style:"acid"});
+	$("pre.sh").snippet("sh",{style:"acid"});
 });
