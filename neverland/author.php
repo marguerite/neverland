915e5a21d25e6fd4f71ceb95f1a2eea89a2e0308
Marguerite Su <i@marguerite.su>
2012-11-05 10:15:44 +0800

    minor fix

--- neverland/author.php
+++ neverland/author.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-			<div class="content">
+<div class="content">
 
 <?php
 	/* Queue the first post, that way we know who
@@ -14,20 +14,32 @@
 		the_post();
 ?>
 
-				<h1 class="page-title author"><?php printf( __( 'Author Archives: %s', 'neverland' ), "<span class='vcard'><a class='url' href='" . get_author_posts_url( get_the_author_meta( 'ID' ) ) . "' title='" . esc_attr( get_the_author() ) . "' rel='me'>" . get_the_author() . "</a></span>" ); ?></h1>
+	<h1 class="page-title author">
+		
+		<?php printf( __( 'Author Archives: %s', 'neverland' ), "<span class='vcard'><a class='url' href='" . get_author_posts_url( get_the_author_meta( 'ID' ) ) . "' title='" . esc_attr( get_the_author() ) . "' rel='me'>" . get_the_author() . "</a></span>" ); ?>
+
+	</h1>
 
 <?php
 // If a user has filled out their description, show a bio on their entries.
 if ( get_the_author_meta( 'description' ) ) : ?>
-					<div id="entry-author-info">
-						<div id="author-avatar">
-							<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'neverland_author_bio_avatar_size', 60 ) ); ?>
-						</div><!-- #author-avatar -->
-						<div id="author-description">
-							<h2><?php printf( __( 'About %s', 'neverland' ), get_the_author() ); ?></h2>
-							<?php the_author_meta( 'description' ); ?>
-						</div><!-- #author-description	-->
-					</div><!-- #entry-author-info -->
+					
+<div id="entry-author-info">
+	<div id="author-avatar">
+							
+		<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'neverland_author_bio_avatar_size', 60 ) ); ?>
+
+	</div><!-- #author-avatar -->
+	
+	<div id="author-description">
+							
+		<h2><?php printf( __( 'About %s', 'neverland' ), get_the_author() ); ?></h2>
+		<?php the_author_meta( 'description' ); ?>
+						
+	</div><!-- #author-description	-->
+
+</div><!-- #entry-author-info -->
+
 <?php endif; ?>
 
 <?php
@@ -43,7 +55,8 @@ if ( get_the_author_meta( 'description' ) ) : ?>
 	 */
 	 get_template_part( 'loop', 'author' );
 ?>
-			</div><!-- #content -->
+			
+</div><!-- #content -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
